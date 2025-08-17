6fd3df0fa3cbe770aa852ab1229affffacb77104
@RupertoTacuche <murcio2490@gmail.com>
2025-08-16 21:51:24 -0600

    segundo respaldo

--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,40 @@ const App = () => {
   return (
   <div className="container">
     <div className="chatbot-popup">
+      {/* Chat Header */}
         <div className="chat-header">
             <div className="header-info">
                 <ChatbotIcon />
 
                 <h2 className="logo-text">Chatbot</h2>
+            </div>
                 <button
                  className="material-symbols-rounded">keyboard_arrow_down</button>
-            </div>
+        </div>
+        {/* Chatbot Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+          <ChatbotIcon />
+          <p className="message-text">
+            Hey there 👋<br /> How can I help you today?
+          </p>
+          </div>
+
+          <div className="message user-message">
+            <p className="message-text">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            </p>
+          </div>
+        </div>
+        {/* Chat Footer */}
+        <div className="chat-footer">
+          <form action="#" className="chat-form">
+            <input type="text" placeholder="Message..." 
+            className="message-input" required />
+            <button
+                 className="material-symbols-rounded">arrow_upward</button>
+          </form>
+
         </div>
     </div>
   </div>
